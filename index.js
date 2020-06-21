801744bc416278193121ca1b84e6d1773221f644
fursov231@hotmail.com <pixar1995q>
2020-06-22 00:50:17 +0300

    first commit

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const keys = require('./keys')
 const errorHandler = require('./middleware/error')
 const fileMiddleware = require('./middleware/file')
 
+
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs',
@@ -42,6 +43,7 @@ app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use('/images', express.static(path.join(__dirname, "images")))
+
 app.use(express.urlencoded({extended: true}))
 app.use(session({
     secret: keys.SESSION_SECRET,
@@ -50,6 +52,7 @@ app.use(session({
     store
 }))
 app.use(fileMiddleware.single('avatar'))
+
 app.use(csrf())
 app.use(flash())
 app.use(helmet())
